48b33d193380d5dd5be9c560e68e114b4f96bfb2
Ryan Haas <rhaas@mcftech.com>
2019-04-30 15:32:58 -0500

    asdf

--- src/App.js
+++ src/App.js
@@ -8,10 +8,8 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          
-          <p>
-            Edit <code>src/App.js</code> and ssssssssave to reload.
-          </p>
+
+          <h1>Testing this out one more time.</h1>
           <a
             className="App-link"
             href="https://reactjs.org"
